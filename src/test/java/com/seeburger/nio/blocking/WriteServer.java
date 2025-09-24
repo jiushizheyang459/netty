d939b68bbeab6b0950452a714f8ee789bfa54121
leeway <leewayhe@qq.com>
2025-09-25 01:20:05 +0800

    nio-selector-bytebuffer大小分配、写入内容过多问题

---
+++ src/test/java/com/seeburger/nio/blocking/WriteServer.java
@@ -0,0 +1,49 @@
+package com.seeburger.nio.blocking;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+
+public class WriteServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.configureBlocking(false);
+
+        Selector selector = Selector.open();
+        ssc.register(selector, SelectionKey.OP_ACCEPT);
+
+        ssc.bind(new InetSocketAddress(8080));
+
+        while (true) {
+            selector.select();
+
+            Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
+            while (iter.hasNext()) {
+                SelectionKey key = iter.next();
+                iter.remove();
+                if (key.isAcceptable()) {
+                    SocketChannel sc = ssc.accept();
+                    sc.configureBlocking(false);
+
+                    // 像客户端发送大量数据
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < 3000000; i++) {
+                        sb.append("a");
+                    }
+                    ByteBuffer buffer = Charset.defaultCharset().encode(sb.toString());
+                    while (buffer.hasRemaining()) {
+                        // write()方法不会一次写完，返回值代表实际写入的字节数
+                        int write = sc.write(buffer);
+                        System.out.println(write);
+                    }
+                }
+            }
+        }
+    }
+}
