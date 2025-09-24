618e71864f129618e1be78e246d19b97e529be3f
helewei <05485@xquant.com>
2025-09-24 17:20:19 +0800

    处理消息边界，附件和扩容

--- src/test/java/com/seeburger/nio/blocking/Client.java
+++ src/test/java/com/seeburger/nio/blocking/Client.java
@@ -2,10 +2,31 @@ package com.seeburger.nio.blocking;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 
 public class Client {
     public static void main(String[] args) throws IOException {
+//        client1();
+        client2();
+    }
+
+    /**
+     * 处理消息边界的测试客户端
+     * @throws IOException
+     */
+    private static void client2() throws IOException {
+        SocketChannel sc = SocketChannel.open();
+        sc.connect(new InetSocketAddress("localhost", 8080));
+        SocketAddress address = sc.getLocalAddress();
+//        sc.write(Charset.defaultCharset().encode("hello\nworld\n"));
+        sc.write(Charset.defaultCharset().encode("0123\n456789abcdef"));
+        sc.write(Charset.defaultCharset().encode("0123456789abcdef3333\n"));
+        System.in.read();
+    }
+
+    private static void client1() throws IOException {
         SocketChannel sc = SocketChannel.open();
         sc.connect(new InetSocketAddress("localhost", 8080));
         System.out.println("waiting...");
--- src/test/java/com/seeburger/nio/blocking/Server.java
+++ src/test/java/com/seeburger/nio/blocking/Server.java
@@ -6,10 +6,12 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import static com.seeburger.bytebuffer.ByteBufferUtil.debugAll;
 import static com.seeburger.bytebuffer.ByteBufferUtil.debugRead;
 
 @Slf4j
@@ -19,7 +21,91 @@ public class Server {
 //        blocking();
 //        processAccept();
 //        cancel();
-        processRead();
+//        processRead();
+        handlingMessageBoundaries();
+    }
+
+    /**
+     * selector
+     * 处理消息边界
+     * @throws IOException
+     */
+    private static void handlingMessageBoundaries() throws IOException {
+        // 创建 processAccept，管理多个channel
+        Selector selector = Selector.open();
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.configureBlocking(false);
+
+        // 建立 processAccept 和 channel 的联系（注册）
+        // SelectionKey 就是将来事件发生后，通过他可以知道事件和哪个channel的事件
+        // ops意思为这个key不关注任何事件，初始化
+        SelectionKey sscKey = ssc.register(selector, 0, null);
+        // 指明这个 key 只关注accept事件
+        sscKey.interestOps(SelectionKey.OP_ACCEPT);
+        log.debug("register key: {}", sscKey);
+
+        ssc.bind(new InetSocketAddress(8080));
+        while (true) {
+            // select 在事件未处理时，不会阻塞
+            selector.select();
+
+            // 处理事件，selectedKeys 内部包含了所有发生的事件
+            Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
+            while (iter.hasNext()) {
+                SelectionKey key = iter.next();
+                //处理key的时候，要从 selectedKeys 集合中删除，否则下次处理就会有问题
+                iter.remove();
+                log.debug("key: {}", key);
+                // 区分事件类型
+                if (key.isAcceptable()) {
+                    ServerSocketChannel channel = (ServerSocketChannel) key.channel();
+                    SocketChannel sc = channel.accept();
+                    sc.configureBlocking(false);
+                    ByteBuffer buffer = ByteBuffer.allocate(16); // attachment
+                    // 将一个ByteBuffer作为附件关联到SelectionKey上
+                    SelectionKey scKey = sc.register(selector, 0, buffer);
+                    scKey.interestOps(SelectionKey.OP_READ);
+                    log.debug("{}", sc);
+                    log.debug("scKey: {}", scKey);
+                } else if (key.isReadable()) {
+                    SocketChannel channel = (SocketChannel) key.channel();
+                    // 获取SelectionKey上关联的附件
+                    ByteBuffer buffer = (ByteBuffer) key.attachment();
+                    int read = channel.read(buffer);
+                    if (read < 0) {
+                        key.cancel(); // 客户端断开了，需要将key取消
+                    } else {
+                        split(buffer);
+                        if (buffer.position() == buffer.limit()) {
+                            ByteBuffer newBuffer = ByteBuffer.allocate(buffer.capacity() * 2);
+                            // 在split()方法中调用compact()切换为了写模式，这里要切换为读模式
+                            buffer.flip();
+                            newBuffer.put(buffer);
+                            key.attach(newBuffer);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static void split(ByteBuffer source) {
+        source.flip();
+        for (int i = 0; i < source.limit(); i++) {
+            // 找到了一条完整消息
+            if (source.get(i) == '\n') {
+                int length = i + 1 - source.position();
+                // 把这条完整消息存入新的 ByteBuffer
+                ByteBuffer target = ByteBuffer.allocate(length);
+
+                // 从 source 读，向 target 写
+                for (int j = 0; j < length; j++) {
+                    target.put(source.get());
+                }
+                debugAll(target);
+            }
+        }
+        source.compact();
     }
 
     /**
@@ -63,13 +149,14 @@ public class Server {
                     log.debug("accept key: {}", sc);
                 } else if (key.isReadable()) {
                     SocketChannel channel = (SocketChannel) key.channel();
-                    ByteBuffer buffer = ByteBuffer.allocate(16);
+                    ByteBuffer buffer = ByteBuffer.allocate(4);
                     int read = channel.read(buffer);
                     if (read < 0) {
                         key.cancel(); // 客户端断开了，需要将key取消
                     } else {
                         buffer.flip();
                         debugRead(buffer);
+//                        System.out.println(Charset.defaultCharset().decode(buffer));
                     }
                 }
             }
