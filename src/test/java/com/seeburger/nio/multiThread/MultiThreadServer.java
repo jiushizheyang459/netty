bcc831a1e2715cf289a8d6f1f82b0cffdc7d4822
leeway <leewayhe@qq.com>
2025-09-26 01:25:32 +0800

    nio多线程优化

--- src/test/java/com/seeburger/nio/multiThread/MultiThreadServer.java
+++ src/test/java/com/seeburger/nio/multiThread/MultiThreadServer.java
@@ -7,6 +7,7 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
 import java.util.Iterator;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import static com.seeburger.bytebuffer.ByteBufferUtil.debugAll;
 
@@ -23,7 +24,6 @@ public class MultiThreadServer {
 
         // 创建固定数量的worker 并初始化
         Worker worker = new Worker("worker-0");
-        worker.register();
 
         while (true) {
             boss.select();
@@ -37,7 +37,7 @@ public class MultiThreadServer {
                     log.debug("connected...{}", sc.getRemoteAddress());
                     // 关联selector
                     log.debug("before register...{}", sc.getRemoteAddress());
-                    sc.register(worker.selector, SelectionKey.OP_READ);
+                    worker.register(sc); // boss线程调用 初始化 selector，启动worker-0
                     log.debug("after register...{}", sc.getRemoteAddress());
 
                 }
@@ -45,32 +45,45 @@ public class MultiThreadServer {
         }
     }
 
-    static class Worker implements  Runnable {
+    static class Worker implements Runnable {
         private Thread thread;
         private Selector selector;
-        private String Name;
-        private boolean strat = false; // 还未初始化
+        private String name;
+        private volatile boolean start = false; // 还未初始化
+        private ConcurrentLinkedQueue<Runnable> queue = new ConcurrentLinkedQueue<>();
 
         public Worker(String name) {
-            Name = name;
+            this.name = name;
         }
 
         // 初始化线程和selector
-        public void register() throws IOException {
-            if (!strat) {
-                thread = new Thread(this, Name);
+        public void register(SocketChannel sc) throws IOException {
+            if (!start) {
+                thread = new Thread(this, name);
                 thread.start();
                 selector = Selector.open();
-                strat = true;
+                start = true;
             }
-
+            // 向队列添加了任务，但任务没有立马执行 boss
+            queue.add(() -> {
+                try {
+                    sc.register(selector, SelectionKey.OP_READ);
+                } catch (ClosedChannelException e) {
+                    e.printStackTrace();
+                }
+            });
+            selector.wakeup(); // 唤醒 select 方法
         }
 
         @Override
         public void run() {
             while (true) {
                 try {
-                    selector.select();
+                    selector.select(); // worker-0 阻塞，wakeup
+                    Runnable task = queue.poll();
+                    if (task != null) {
+                        task.run(); // 执行了sc.register(selector, SelectionKey.OP_READ);
+                    }
                     Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
                     while (iter.hasNext()) {
                         SelectionKey key = iter.next();
