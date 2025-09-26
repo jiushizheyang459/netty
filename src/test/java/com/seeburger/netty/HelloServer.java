1e062bf348addda34ecf5a6cf7f7d60996e4d90e
jiushizheyang <lewei.he@xquant.com>
2025-09-26 17:57:54 +0800

    netty server

---
+++ src/test/java/com/seeburger/netty/HelloServer.java
@@ -0,0 +1,35 @@
+package com.seeburger.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+public class HelloServer {
+    public static void main(String[] args) {
+        // 启动器，负责组装netty组件，启动服务器
+        new ServerBootstrap()
+                // BossEventLoop,WorkerEventLoop(selector,thread),group组
+                .group(new NioEventLoopGroup())
+                // 选择服务器的 ServerSocketChannel
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                System.out.println(msg);
+                            }
+                        });
+                    }
+                })
+                .bind(8080);
+    }
+}
