8b90534f4cfde4f397e0184f5b06618442cf07a3
leeway <leewayhe@qq.com>
2025-09-29 00:09:15 +0800

    netty client

---
+++ src/test/java/com/seeburger/netty/HelloClient.java
@@ -0,0 +1,32 @@
+package com.seeburger.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+
+public class HelloClient {
+    public static void main(String[] args) throws InterruptedException {
+        // 创建启动器
+        new Bootstrap()
+                // 添加EventLoop
+                .group(new NioEventLoopGroup())
+                // 选择客户端channel实现
+                .channel(NioSocketChannel.class)
+                // 添加处理器
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override // 在连接建立后被调用
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                .connect(new InetSocketAddress("localhost", 8080))
+                .sync()
+                .channel()
+                // 向服务器发送数据
+                .writeAndFlush("hello,world");
+    }
+}
--- src/test/java/com/seeburger/netty/HelloServer.java
+++ src/test/java/com/seeburger/netty/HelloServer.java
@@ -18,18 +18,24 @@ public class HelloServer {
                 .group(new NioEventLoopGroup())
                 // 选择服务器的 ServerSocketChannel
                 .channel(NioServerSocketChannel.class)
-                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                // boss负责处理worker(child) 负责读写 决定了worker(child)能执行哪些操作(handler)
+                .childHandler(
+                        // channel代表和客户端进行读写的通道 Initializer 初始化，负责添加别的 handler
+                        new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
-                        ch.pipeline().addLast(new StringDecoder());
-                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
-                            @Override
+                        // 添加具体的handler
+                        ch.pipeline().addLast(new StringDecoder()); // 将 ByteBuf 转为字符串
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() { // 自定义handler
+                            @Override // 读事件
                             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                // 打印上一步转换好的字符串
                                 System.out.println(msg);
                             }
                         });
                     }
                 })
+                // 绑定监听端口
                 .bind(8080);
     }
 }
