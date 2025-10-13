4592e7c0a31ff191766e24e1316bc846a6afea01
leeway <leewayhe@qq.com>
2025-10-14 00:46:17 +0800

    netty eventloop io任务

---
+++ src/test/java/com/seeburger/netty/eventloop/EventLoopClient.java
@@ -0,0 +1,33 @@
+package com.seeburger.netty.eventloop;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+
+public class EventLoopClient {
+    public static void main(String[] args) throws InterruptedException {
+        // 创建启动器
+        Channel channel = new Bootstrap()
+                // 添加EventLoop
+                .group(new NioEventLoopGroup())
+                // 选择客户端channel实现
+                .channel(NioSocketChannel.class)
+                // 添加处理器
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override // 在连接建立后被调用
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                .connect(new InetSocketAddress("localhost", 8080))
+                .sync()
+                .channel();
+        System.out.println(channel);
+        System.out.println("");
+    }
+}
