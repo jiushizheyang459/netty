bf59888927c6289d2c6907b96e794d4377e8514f
leeway <leewayhe@qq.com>
2025-10-15 01:16:30 +0800

    netty eventloop 分工细化

--- src/test/java/com/seeburger/netty/eventloop/EventLoopServer.java
+++ src/test/java/com/seeburger/netty/eventloop/EventLoopServer.java
@@ -16,7 +16,9 @@ import java.nio.charset.Charset;
 public class EventLoopServer {
     public static void main(String[] args) {
         new ServerBootstrap()
-                .group(new NioEventLoopGroup())
+                // boss 和 worker
+                // boss 只负责 serverSocketChannel 上的 accept事件         worker 只负责 socketChannel 上的读写
+                .group(new NioEventLoopGroup(), new NioEventLoopGroup(2))
                 .channel(NioServerSocketChannel.class)
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
